6378f2d545cc316ddab05b0b55874658921bc94a
Zhu Wang <zw401@nyu.edu>
2013-05-10 00:29:14 -0400

    Harris Detection implemented

---
+++ HarrisDetection/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+ 
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+ 
+#pragma warning( disable: 4996 )
+ 
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+ 
+#include "CLEyeMulticam.h"
+
+#include <opencv\cv.h>
+#include <opencv\highgui.h>
